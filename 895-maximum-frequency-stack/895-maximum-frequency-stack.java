5004341b478cc229d411c49fe7716f629611226e
jasleen0604 <66011969+jasleen0604@users.noreply.github.com>
2022-03-29 18:06:27 +0530

    Time: 26 ms (96.37%), Space: 50.8 MB (95.22%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,34 +1,59 @@
 class FreqStack {
 
- HashMap<Integer,Integer> frequency;
-    HashMap<Integer,Stack<Integer>> individual;
-    int maxfreq;
-
+    HashMap<Integer, Stack<Integer>> hm = new HashMap<>();
+    HashMap<Integer,Integer> freqmap = new HashMap<>();
+    
     public FreqStack() {
-           maxfreq=0;
-        frequency=new HashMap<>();
-        individual=new HashMap<>();
+        hm = new HashMap<>();
+        freqmap = new HashMap<>();
     }
     
     public void push(int val) {
-        frequency.put(val,frequency.getOrDefault(val,0)+1);
-        if(!individual.containsKey(frequency.get(val))){
-            individual.put(frequency.get(val),new Stack<Integer>());
+        int idx = 1;
+        if(freqmap.containsKey(val))
+        {
+            idx = freqmap.get(val);
+            idx++;
+            freqmap.put(val,idx);
         }
-        individual.get(frequency.get(val)).add(val) ;
-        maxfreq = maxfreq > frequency.get(val) ? maxfreq : frequency.get(val);
-     
+        else
+            freqmap.put(val,1);
         
+        if(hm.containsKey(idx))
+        {
+            Stack<Integer> st = hm.get(idx);
+            st.push(val);
+            hm.put(idx,st);
+        }
+        else
+        {
+            Stack<Integer> st = new Stack<>();
+            st.push(val);
+            hm.put(idx,st);
+        }
     }
     
     public int pop() {
-        int topElement = individual.get(maxfreq).pop();
-        if(individual.get(maxfreq).isEmpty()){
-            maxfreq--;
-        }
-        frequency.put(topElement,frequency.getOrDefault(topElement,0)-1);
-
-        return topElement;
-       
+        Stack<Integer> st = hm.get(hm.size());
+        int res = st.pop();
+        
+        if(freqmap.get(res) == 1)
+            freqmap.remove(res);
+        else
+            freqmap.put(res,freqmap.get(res)-1);
+        
+        if(st.size()== 0)
+        hm.remove(hm.size());
+        
+        return res;
+        
+        
     }
 }
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
